1e638959ae8f23c3d30ece9dd57fc81289a85319
datmanydocris <datmanydocris@hotmail.com>
2026-08-28 11:06:35 -0400

    H: Text to TextNode implemented

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,6 +1,6 @@
 import unittest
 from textnode import TextNode, TextType
-from inline_markdown import split_nodes_delimiter, split_nodes_image, split_nodes_link
+from inline_markdown import split_nodes_delimiter, split_nodes_image, split_nodes_link, text_to_textnodes
 from inline_markdown import (
     extract_markdown_images,
     extract_markdown_links,
@@ -204,6 +204,33 @@ class TestSplitImagesAndLinks(unittest.TestCase):
         node = TextNode("already bold", TextType.BOLD)
         new_nodes = split_nodes_image([node])
         self.assertListEqual([node], new_nodes)
+
+class TestTextToTextNodes(unittest.TestCase):
+    def test_text_to_textnodes(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        nodes = text_to_textnodes(text)
+        self.assertListEqual(
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ],
+            nodes,
+        )
+
+    def test_text_to_textnodes_plain(self):
+        nodes = text_to_textnodes("just plain text")
+        self.assertListEqual(
+            [TextNode("just plain text", TextType.TEXT)],
+            nodes,
+        )
 
 
 if __name__ == "__main__":
